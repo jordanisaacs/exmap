e725fd3c17573a54fda861a44ae3600f40666b78
Jordan Isaacs <mail@jdisaacs.com>
2022-12-18 20:41:58 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    kernel = pkgs.linuxPackages_latest.kernel;
+
+    exmap = pkgs.stdenv.mkDerivation {
+      name = "exmap";
+      src = ./module;
+      outputs = ["out" "dev"];
+
+      buildInputs = [pkgs.nukeReferences];
+      kernel = kernel.dev;
+      kernelVersion = kernel.modDirVersion;
+
+      buildPhase = ''
+        make -s "KDIR=$kernel/lib/modules/$kernelVersion/build" modules
+      '';
+
+      installPhase = ''
+        mkdir -p $out/lib/modules/$kernelVersion/misc
+        for x in $(find . -name '*.ko'); do
+          nuke-refs $x
+          cp $x $out/lib/modules/$kernelVersion/misc/
+        done
+
+        mkdir -p $dev/include
+        cp -r linux $dev/include
+      '';
+    };
+  in {
+    packages.x86_64-linux = {
+      inherit exmap;
+      default = exmap;
+    };
+  };
+}
