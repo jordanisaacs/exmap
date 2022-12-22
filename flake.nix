abedcf88676912ede5d846927c7ee6888491d818
Jordan Isaacs <mail@jdisaacs.com>
2022-12-22 15:28:27 -0500

    exmap builder

--- flake.nix
+++ flake.nix
@@ -9,33 +9,37 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-    kernel = pkgs.linuxPackages_latest.kernel;
+    buildExmap = kernel:
+      pkgs.stdenv.mkDerivation {
+        name = "exmap";
+        src = ./module;
+        outputs = ["out" "dev"];
 
-    exmap = pkgs.stdenv.mkDerivation {
-      name = "exmap";
-      src = ./module;
-      outputs = ["out" "dev"];
+        buildInputs = [pkgs.nukeReferences];
+        kernel = kernel.dev;
+        kernelVersion = kernel.modDirVersion;
 
-      buildInputs = [pkgs.nukeReferences];
-      kernel = kernel.dev;
-      kernelVersion = kernel.modDirVersion;
+        buildPhase = ''
+          make -s "KDIR=$kernel/lib/modules/$kernelVersion/build" modules
+        '';
 
-      buildPhase = ''
-        make -s "KDIR=$kernel/lib/modules/$kernelVersion/build" modules
-      '';
+        installPhase = ''
+          mkdir -p $out/lib/modules/$kernelVersion/misc
+          for x in $(find . -name '*.ko'); do
+            nuke-refs $x
+            cp $x $out/lib/modules/$kernelVersion/misc/
+          done
 
-      installPhase = ''
-        mkdir -p $out/lib/modules/$kernelVersion/misc
-        for x in $(find . -name '*.ko'); do
-          nuke-refs $x
-          cp $x $out/lib/modules/$kernelVersion/misc/
-        done
+          mkdir -p $dev/include
+          cp -r linux $dev/include
+        '';
+      };
 
-        mkdir -p $dev/include
-        cp -r linux $dev/include
-      '';
-    };
+    exmap = buildExmap pkgs.linuxPackages_latest.kernel;
   in {
+    lib = {
+      inherit buildExmap;
+    };
     packages.x86_64-linux = {
       inherit exmap;
       default = exmap;
